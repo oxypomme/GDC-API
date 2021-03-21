d9accd1617c7d8cdc2b08cd7ef99e0ab7b793d86
OxyTom <oxytom8@gmail.com>
2021-03-21 12:31:05 +0100

    finished missions endpoint

--- api/missions.js
+++ api/missions.js
@@ -27,7 +27,32 @@ const getIntStatus = (status) => {
  * @apiDescription Gets the informations about missions
  * 
  * @apiSuccess {JSONArray} result The missions infos
-
+ * [
+ *     {
+ *         "id": "1617",
+ *         "name": "CPC-CO[19]-Matinee_brumeuse-V6",
+ *         "creation_date": "Podagorsk",
+ *         "formation": "20/03/2021",
+ *         "count_missions": "62",
+ *         "last_mission": "Inconnu"
+ *     },
+ *     {
+ *         "id": "1616",
+ *         "name": "CPC-CO[04]-Piece_de_8-v2",
+ *         "creation_date": "Stratis",
+ *         "formation": "20/03/2021",
+ *         "count_missions": "14",
+ *         "last_mission": "ECHEC"
+ *     },
+ *     {
+ *         "id": "1615",
+ *         "name": "CPC-CO[20]-Veine_de_Cobra-v1",
+ *         "creation_date": "Desert",
+ *         "formation": "19/03/2021",
+ *         "count_missions": "68",
+ *         "last_mission": "SUCCES"
+ *     }
+ * ]
  */
 app.get('/gdc/missions', async (req, res) => {
     const { document: doc } = new JSDOM(await (await fetch(`https://grecedecanards.fr/GDCStats/missions`)).text(), {
@@ -58,8 +83,34 @@ app.get('/gdc/missions', async (req, res) => {
  * @apiName GetPlayersById
  * @apiGroup Players
  * @apiDescription Gets the informations about the player
- * TODO
+ *
  * @apiSuccess {JSONObject} result The player infos and missions
+ * {
+ *     "infos": {
+ *         "id": 1617,
+ *         "name": "CPC-CO[19]-Matinee_brumeuse-V6",
+ *         "map": "Podagorsk",
+ *         "date": "20/03/2021",
+ *         "duration": "62",
+ *         "status": 0,
+ *         "players": 15,
+ *         "end_players": 6
+ *     },
+ *     "missions": [
+ *         {
+ *             "id": 40,
+ *             "name": "Sardo",
+ *             "role": "Officier (148 + 117)",
+ *             "status": "Mort"
+ *         },
+ *         {
+ *             "id": 271,
+ *             "name": "Thétard",
+ *             "role": "Infirmier",
+ *             "status": "Mort"
+ *         }
+ *     ]
+ * }
  */
 app.get('/gdc/missions/:id', async (req, res) => {
     const { id } = req.params;
@@ -67,42 +118,39 @@ app.get('/gdc/missions/:id', async (req, res) => {
         url: `https://grecedecanards.fr/GDCStats/missions/${id}`
     }).window;
 
-    let missions;
+    let players;
     let infos;
 
-    const player = doc.querySelector("#page-wrapper table:first-of-type tbody").children[0];
-    if (player) {
+    const mission = doc.querySelector("#page-wrapper table:first-of-type tbody").children[0];
+    if (mission) {
         infos = {
             id: parseInt(id),
-            name: player.children[0].innerHTML,
-            creation_date: player.children[1].innerHTML,
-            formation: player.children[2].innerHTML,
-            count_missions: parseInt(player.children[3].innerHTML)
+            name: mission.children[1].children[0].innerHTML,
+            map: mission.children[2].innerHTML,
+            date: mission.children[3].innerHTML,
+            duration: mission.children[4].innerHTML,
+            status: getIntStatus(mission.children[5].innerHTML),
+            players: parseInt(mission.children[6].innerHTML),
+            end_players: parseInt(mission.children[7].innerHTML)
         };
     }
 
     const table = doc.querySelector("#page-wrapper table:last-of-type tbody");
     if (table) {
-        missions = [];
+        players = [];
         for (const row of table.children) {
             const match = /(.*\/)(.*)/.exec(row.children[0].children[0].href);
-            missions.push({
+            players.push({
                 id: parseInt(match[match.length - 1]),
                 name: row.children[0].children[0].innerHTML,
-                map: row.children[1].innerHTML,
-                date: row.children[2].innerHTML,
-                duration: parseInt(row.children[3].innerHTML),
-                mission_status: getIntStatus(row.children[4].innerHTML),
-                players: parseInt(row.children[5].innerHTML),
-                end_players: parseInt(row.children[6].innerHTML),
-                role: row.children[7].innerHTML,
-                player_status: getIntStatus(row.children[8].innerHTML),
+                role: row.children[1].innerHTML,
+                status: row.children[2].innerHTML
             });
         }
     }
 
     res.status(200).json({
         infos,
-        missions
+        missions: players
     });
 })
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const { app } = require('./app');
 
 require('./api/players');
+require('./api/missions');
 
 app.use('/gdc/', express.static(__dirname + '/doc'));
